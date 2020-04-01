948c8b36fb671a63420635432f651e457b3f1b5e
zouzhicun <2515717245@qq.com>
2020-04-01 20:02:21 +0900

    file proxy

--- core/api/fileproxy.py
+++ core/api/fileproxy.py
@@ -3,69 +3,81 @@ import os
 import re
 import sys
 import time
+import json
+import requests
+from traceback import format_exc
+
 from rest_framework.response import Response
 from django.http import FileResponse
 
 from auth_new import baseview
 from libs import util
 from conf import config
-
+from libs.util import MYLOGGER,MYLOGERROR
 from libs.wrapper import error_capture,file_root,playbook_root
 
-#实现对核心后端文件处理接口的转发
-class File(baseview.BaseView):
+solve_host="192.168.253.128"
+solve_port=9000
+
+#curl  "http://127.0.0.1:8000/api/v1/file/?path=./" -F "file=@/root/xxx.sh"
+#real_url='http://127.0.0.1:9000/api/v1/file/?path=/tmp/a/b/c'
+#file = {'file': open("/home/weideguo/get-pip.py", 'rb')}
+#response = requests.post(real_url,headers=headers, files=files).text
+
+def result_parse(res,msg1,msg2):
+    r=None
+    try:
+        r_text=res.text
+        r=json.loads(r_text)
+        if "status" in r and r["status"]>0:
+            MYLOGERROR.error(r_text)
+            r['msg']=util.safe_decode(msg1)
+        else:
+            r=None
+    except:
+        MYLOGERROR.error(format_exc())
+    if not r:
+        r={'status':-1,'msg':util.safe_decode(msg2)}
+    return r
+
+
+class Test(baseview.AnyLogin):
     '''
-    关于文件的操作
+    #实现对核心后端文件处理接口的转发
     中文路径 文件名支持 export LC_ALL=en_US.UTF-8
     '''
+    base_url='http://%s:%s/file/' % (solve_host,solve_port)
+
     @error_capture
     def post(self, request, args = None):
         fr=request.FILES.get('file',None)      #curl "$url" -F "file=@/root/x.txt"  
-        path=request.GET['path']    
-        filename=fr.name
-        #print filename 
-        save_path=os.path.join(file_root,'./'+path)
-        if os.path.isfile(save_path):
-            msg=util.safe_decode('路径为文件')
-            status=-1
-            return Response({'file':save_path,'msg':msg,'status':status})
-        elif not os.path.exists(save_path):
-            os.makedirs(save_path)
-
-        full_path=os.path.join(save_path,filename)
-        #文件存在则重命名已经存在的文件
-        if os.path.isfile(full_path):
-            os.rename(full_path,full_path+'_'+str(time.time())) 
+        path=request.GET['path']   
+        path=os.path.join(file_root,'./'+path)
         
-        with open(full_path,'wb') as f:
-            #防止上传文件太大 分块读写到磁盘
-            for chunk in fr.chunks():
-               f.write(chunk) 
+        #help(fr)
+        file = {'file': fr}
+        url=self.base_url+"?path="+path
+        r = requests.post(url, files=file)
+        
+        r=result_parse(r,'上传成功','上传出现错误，请查看日志')
 
-            status=1
-            msg=util.safe_decode('上传成功')
-
-        return Response({'status':status,'file':full_path,'msg':msg})
-
+        return Response(r)
 
     @error_capture
     def get(self, request, args = None):
+
+        self.base_url=self.base_url+args
         if args == 'content':
             filename = request.GET['file']
             #是否是相对路径
             
             filename = os.path.join(playbook_root,filename)
             
-            content=''
-            try:
-                with open(filename) as f:
-                    content=f.read()
-
-                return Response({'status':1,'file':filename,'content':util.safe_decode(content)})
-            except:
-                return Response({'status':-1,'file':filename,'msg':util.safe_decode('读取文件失败')})
-
-        
+            url=self.base_url+"?file="+filename
+            r = requests.get(url)
+            r=result_parse(r,'读取成功','读取失败，请查看日志')
+            return Response(r)
+                
         if args == 'download':
             filename = request.GET['file']
             name=os.path.basename(filename)
@@ -74,48 +86,37 @@ class File(baseview.BaseView):
             #attachment下载文件, inline 和 attachment inline 在浏览器中显示
             showtype = ['attachment','inline', 'attachment inline'][0]
 
-            if os.path.isfile(filename):
-                file = open(filename,mode='rb')
-                response=FileResponse(file)
+            url=self.base_url+"?file="+filename
+            r = requests.get(url)
+
+            try:
+                response=FileResponse(r.content)
                 response['Content-Type']='application/octet-stream'
                 response['Content-Disposition']='%s;filename=%s' % (showtype, name.encode('utf8'))   
                 return response
-            else:
-                return Response({'status':-1,'file':filename,'msg':util.safe_decode('路径不为文件')})
+            except:
+                print(r.text)
+                r=result_parse(r,'下载成功','下载失败，请查看日志')
+                return Response(r)
        
 
         if args == 'list':
             root_path = request.GET['path']
             root_path = os.path.join(file_root,'./'+root_path)
-            files=[]
-            dirs=[]
             
-            if not os.path.isfile(root_path) and os.path.exists(root_path):
-                for x in os.listdir(root_path):
-                    if os.path.isfile(os.path.join(root_path,x)):
-                        files.append(x)
-                    else:
-                        dirs.append(x)
-            
-                files.sort()
-                dirs.sort()
-                return Response({'status':1,'path':root_path,'files':files,'dirs':dirs})
-            else:
-                return Response({'status':-1,'path':root_path,'msg':util.safe_decode('路径不为目录')}) 
-
+            url=self.base_url+"?path="+root_path
+            r = requests.get(url)
+            r=result_parse(r,'查看成功','查看失败，请查看日志')
+            return Response(r)
 
         if args == 'create':
             create_path = request.GET['path']
             create_path = os.path.join(file_root,'./'+create_path)
-            try:
-                os.makedirs(create_path)
-                status=1
-                msg=util.safe_decode('创建成功')
-            except OSError:
-                status=-1
-                msg=util.safe_decode('创建失败')
-
-            return Response({'status':status,'path':create_path,'msg':msg})
+            
+            url=self.base_url+"?path="+create_path
+            r = requests.get(url)
+            r=result_parse(r,'创建成功','创建失败，请查看日志')
+            return Response(r)
 
 
 
