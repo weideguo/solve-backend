3d1223c4a684ac415e6d0f9fb1704adccdb7d033
zouzhicun <2515717245@qq.com>
2021-01-12 11:36:14 +0900

    update

--- setting/wsgi.py
+++ setting/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for auto_deploy project.
+WSGI config for solve-backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
