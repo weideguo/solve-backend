4f6885784b528e38c86657c7c5384b246cbfe9db
weideguo <2515717245@qq.com>
2021-09-30 16:33:06 +0800

    chore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,12 @@
 # export REDIS_PASSWORD=my_redis_passwd
 #
 cd /data/solve-backend
-sed -i "s|127.0.0.1|${REDIS_HOST}|g" ./deploy.conf
-sed -i "s|6379|${REDIS_PORT}|g" ./deploy.conf
 sed -i "s|my_redis_passwd|${REDIS_PASSWORD}|g" ./deploy.conf
+if [ "X${REDIS_HOST}X" != "XX" ]; then sed -i "s|host=127.0.0.1|host=${REDIS_HOST}|g" ./deploy.conf;fi
+if [ "X${REDIS_PORT}X" != "XX" ]; then sed -i "s|6379|${REDIS_PORT}|g" ./deploy.conf;fi
+if [ "X${SECRET_KEY}X" != "XX" ]; then 
+SECRET_KEY=`echo ${SECRET_KEY} | sed "s|&|\\\\\\&|g"` \
+&& sed -i "s|^SECRET_KEY.*|SECRET_KEY = '${SECRET_KEY}'|g"  ./setting/settings.py
+fi
 python set_config.py   
 python manage.py runserver 0.0.0.0:8000
