7e3aea4eda3f27bb267a08f7686bb73a854715d1
weideguo <2515717245@qq.com>
2023-08-15 19:40:24 +0800

    chore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,12 @@
 #
 cd /data/solve-backend
 sed -i "s|my_redis_passwd|${REDIS_PASSWORD}|g" ./deploy.conf
-if [ "X${REDIS_HOST}X" != "XX" ]; then sed -i "s|host=127.0.0.1|host=${REDIS_HOST}|g" ./deploy.conf;fi
-if [ "X${REDIS_PORT}X" != "XX" ]; then sed -i "s|6379|${REDIS_PORT}|g" ./deploy.conf;fi
-if [ "X${SECRET_KEY}X" != "XX" ]; then 
+if [[ -n ${REDIS_HOST} ]]; then sed -i "s|host=127.0.0.1|host=${REDIS_HOST}|g" ./deploy.conf;fi
+if [[ -n ${REDIS_PORT} ]]; then sed -i "s|6379|${REDIS_PORT}|g" ./deploy.conf;fi
+if [[ -n ${MONGO_URI}  ]]; then sed -i "s|#uri=.*|uri=${MONGO_URI}|g" ./deploy.conf;fi
+if [[ -n ${MONGO_DB}   ]]; then sed -i "s|#db=solve|db=${MONGO_DB}|g" ./deploy.conf;fi
+if [[ -n ${DATA_PATH}  ]]; then sed -i "s|/tmp/|${DATA_PATH}|g" ./deploy.conf;fi
+if [[ -n ${SECRET_KEY} ]]; then 
 SECRET_KEY=`echo ${SECRET_KEY} | sed "s|&|\\\\\\&|g"` \
 && sed -i "s|^SECRET_KEY.*|SECRET_KEY = '${SECRET_KEY}'|g"  ./setting/settings.py
 fi
